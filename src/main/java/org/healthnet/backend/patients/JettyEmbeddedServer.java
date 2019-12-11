aad87c05dcfb7f23f0911f2ee13db2899e9ec492
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-12-11 10:21:01 +0100

    Implemented Jetty Server

---
+++ src/main/java/org/healthnet/backend/patients/JettyEmbeddedServer.java
@@ -0,0 +1,27 @@
+package org.healthnet.backend.patients;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.Servlet;
+
+public class JettyEmbeddedServer {
+    private final Server server;
+
+    public JettyEmbeddedServer(int port, Servlet servlet) {
+        server = new org.eclipse.jetty.server.Server(port);
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        ServletHolder servletHolder = new ServletHolder("application", servlet);
+        contextHandler.addServlet(servletHolder, "/");
+        server.setHandler(contextHandler);
+    }
+
+    public void start() throws Exception {
+        server.start();
+    }
+
+    public void stop() throws Exception {
+        server.stop();
+    }
+}
---
+++ src/test/java/org/healthnet/backend/patients/JettyEmbeddedServerTest.java
@@ -0,0 +1,62 @@
+package org.healthnet.backend.patients;
+
+import org.healthnet.backend.patients.JettyEmbeddedServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JettyEmbeddedServerTest {
+    private static final int SERVER_PORT = 8080;
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final HttpServlet servlet = new TestServlet();
+    private static final JettyEmbeddedServer server = new JettyEmbeddedServer(SERVER_PORT, servlet);
+
+    @Test
+    void Invoke_Root_ResponseHasBeenReturned() throws Exception {
+        String body = get("/");
+        assertEquals("Hello!", body);
+    }
+
+    @Test
+    void Invoke_ArbitraryPath_ResponseHasBeenReturned() throws Exception {
+        String body = get("/foo");
+        assertEquals("Hello!", body);
+    }
+
+    private String get(String path) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:" + SERVER_PORT + path))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        server.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws Exception {
+        server.stop();
+    }
+
+    public static class TestServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            resp.getWriter().write("Hello!");
+        }
+    }
+}

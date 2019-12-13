c9fb64c7868bc1dc5e93a00ac90b615179f4aeba
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-12-13 11:21:33 +0100

    Added datasource initialization in main

--- src/main/java/org/healthnet/backend/patients/Main.java
+++ src/main/java/org/healthnet/backend/patients/Main.java
@@ -1,29 +1,34 @@
 package org.healthnet.backend.patients;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.healthnet.backend.patients.application.services.PatientRegistrationService;
 import org.healthnet.backend.patients.domain.patient.Patient;
 import org.healthnet.backend.patients.domain.patient.PatientRepository;
 import org.healthnet.backend.patients.infrastructure.persistence.PatientPersistenceRepository;
-import org.healthnet.backend.patients.infrastructure.persistence.PatientsDataSource;
-import org.healthnet.backend.patients.presentation.rest.JettyEmbeddedServer;
-import org.healthnet.backend.patients.presentation.rest.PatientsServlet;
-import org.healthnet.backend.patients.presentation.rest.RegistrationDataMapping;
+import org.healthnet.backend.patients.presentation.rest.PatientRegistrationWebHandler;
+import org.healthnet.backend.patients.presentation.rest.WebHandler;
+import org.healthnet.backend.patients.presentation.tools.jetty.JettyEmbeddedServer;
+import org.healthnet.backend.patients.presentation.tools.jetty.PatientsServlet;
 
 import javax.servlet.http.HttpServlet;
-import javax.sql.DataSource;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        DataSource dataSource = new PatientsDataSource();
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName("healthnet_patients");
+        dataSource.setUser(System.getenv().getOrDefault("HEALTHNET_DB_USER", "root"));
+        dataSource.setPassword(System.getenv().getOrDefault("HEALTHNET_DB_PASSWORD", "root"));
         PatientRepository patientRepository = new PatientPersistenceRepository(dataSource);
         Consumer<PatientRegistrationService.RegistrationData> patientRegistrationService = new PatientRegistrationService(patientRepository, input -> new Patient(
                 new Patient.Id(input.id),
                 new Patient.FullName(input.fullName)
         ));
-        HttpServlet patientsServlet = new PatientsServlet(patientRegistrationService, new RegistrationDataMapping());
+        WebHandler patientRegistrationWebHandler = new PatientRegistrationWebHandler(patientRegistrationService);
+        HttpServlet patientsServlet = new PatientsServlet(patientRegistrationWebHandler);
+
         int port = Integer.parseInt(System.getenv().getOrDefault("HEALTHNET_PORT", "8080"));
         ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         servletContextHandler.addServlet(new ServletHolder(patientsServlet), "/patients");

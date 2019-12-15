442a68609c7f962147361e343842bd36c73a2875
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-12-15 19:16:41 +0100

    Refactored Main

--- src/main/java/org/healthnet/backend/patients/Main.java
+++ src/main/java/org/healthnet/backend/patients/Main.java
@@ -1,56 +1,65 @@
 package org.healthnet.backend.patients;
 
 import com.google.gson.Gson;
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.healthnet.backend.patients.application.dtos.PatientDetailDto;
 import org.healthnet.backend.patients.application.dtos.PatientSummaryDto;
-import org.healthnet.backend.patients.application.dtos.PatientRegistrationDto;
 import org.healthnet.backend.patients.application.services.PatientDetailService;
 import org.healthnet.backend.patients.application.services.PatientRegisterService;
 import org.healthnet.backend.patients.application.services.PatientRegistrationService;
 import org.healthnet.backend.patients.domain.patient.Patient;
 import org.healthnet.backend.patients.domain.patient.PatientRepository;
 import org.healthnet.backend.patients.infrastructure.persistence.PatientPersistenceRepository;
+import org.healthnet.backend.patients.infrastructure.persistence.tools.PatientsDataSource;
 import org.healthnet.backend.patients.presentation.rest.*;
 import org.healthnet.backend.patients.presentation.tools.jetty.JettyEmbeddedServer;
 import org.healthnet.backend.patients.presentation.tools.jetty.PatientsServlet;
 
 import javax.servlet.http.HttpServlet;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import javax.sql.DataSource;
 
 public class Main {
     public static void main(String[] args) {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setDatabaseName("healthnet_patients");
-        dataSource.setUser(System.getenv().getOrDefault("HEALTHNET_DB_USER", "root"));
-        dataSource.setPassword(System.getenv().getOrDefault("HEALTHNET_DB_PASSWORD", "root"));
+        DataSource dataSource = new PatientsDataSource();
         PatientRepository patientRepository = new PatientPersistenceRepository(dataSource);
-        Consumer<PatientRegistrationDto> patientRegistrationService = new PatientRegistrationService(patientRepository, input -> new Patient(
-                new Patient.Id(input.id),
-                new Patient.FullName(input.fullName)
-        ));
-        Supplier<List<PatientSummaryDto>> patientRegisterService = new PatientRegisterService(patientRepository, patient -> new PatientSummaryDto(
-                patient.getId().getValue(),
-                patient.getFullName().getValue()
-        ));
-        Function<String, PatientDetailDto> patientDetailService = new PatientDetailService(
+        var patientRegistrationService = new PatientRegistrationService(
                 patientRepository,
-                (s) -> new Patient.Id(s),
+                (patientRegistrationDto -> new Patient(
+                        new Patient.Id(patientRegistrationDto.id),
+                        new Patient.FullName(patientRegistrationDto.fullName)
+                ))
+        );
+        var patientRegisterService = new PatientRegisterService(
+                patientRepository,
+                patient -> new PatientSummaryDto(
+                        patient.getId().getValue(),
+                        patient.getFullName().getValue()
+                )
+        );
+        var patientDetailService = new PatientDetailService(
+                patientRepository,
+                Patient.Id::new,
                 (patient) -> new PatientDetailDto(
                         patient.getId().getValue(),
                         patient.getFullName().getValue()
                 )
         );
         WebHandler patientRegistrationWebHandler = new PatientRegistrationWebHandler(patientRegistrationService);
-        WebHandler patientRegisterWebHandler = new PatientRegisterWebHandler(patientRegisterService, patientSummaryData -> new Gson().newBuilder().setPrettyPrinting().create().toJson(patientSummaryData));
-        WebHandler patientDetailWebHandler = new PatientDetailWebHandler(patientDetailService, patientDetailDto -> new Gson().newBuilder().setPrettyPrinting().create().toJson(patientDetailDto));
+        WebHandler patientRegisterWebHandler = new PatientRegisterWebHandler(
+                patientRegisterService,
+                patientSummaryData -> new Gson().newBuilder().setPrettyPrinting().create().toJson(patientSummaryData)
+        );
+        WebHandler patientDetailWebHandler = new PatientDetailWebHandler(
+                patientDetailService,
+                patientDetailDto -> new Gson().newBuilder().setPrettyPrinting().create().toJson(patientDetailDto)
+        );
 
-        WebHandler router = new PatientRouter(patientRegistrationWebHandler, patientRegisterWebHandler, patientDetailWebHandler);
+        WebHandler router = new PatientRouter(
+                patientRegistrationWebHandler,
+                patientRegisterWebHandler,
+                patientDetailWebHandler
+        );
 
         HttpServlet patientsServlet = new PatientsServlet(router);
 
---
+++ src/main/java/org/healthnet/backend/patients/infrastructure/persistence/tools/PatientsDataSource.java
@@ -0,0 +1,14 @@
+package org.healthnet.backend.patients.infrastructure.persistence.tools;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.util.Map;
+
+public class PatientsDataSource extends MysqlDataSource {
+    public PatientsDataSource() {
+        setDatabaseName("healthnet_patients");
+        Map<String, String> env = System.getenv();
+        setUser(env.getOrDefault("HEALTHNET_DB_USER", "root"));
+        setPassword(env.getOrDefault("HEALTHNET_DB_PASSWORD", "root"));
+    }
+}
